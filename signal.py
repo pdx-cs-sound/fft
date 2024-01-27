b304b0fd4fb2b2dfb1c306e4eae73bad72928b33
Bart Massey <bart.massey@gmail.com>
2024-01-26 16:49:10 -0800

    fixed signal.py to have multiple saw sources and phase control for everything

--- signal.py
+++ signal.py
@@ -5,8 +5,10 @@ import scipy.io.wavfile as wav
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--volume", help="volume 0..10", type=float, default=8)
 ap.add_argument("-o", "--output", help="output file")
-ap.add_argument("-f", "--frequency", type=float, default=1000)
-ap.add_argument("signal", help="signal type", choices=["sine", "saw-rise", "saw-fall"])
+ap.add_argument("-f", "--frequency", help="fundamental frequency", type=float, default=1000)
+ap.add_argument("-p", "--phase", help="initially rise or fall", choices=["rise", "fall"], default="rise")
+ap.add_argument("-a", "--algorithm", help="saw algorithm", choices=["segments", "mod", "floor"], default="floor")
+ap.add_argument("signal", help="signal type", choices=["sine", "saw"])
 ap.add_argument("duration", help="seconds to play", type=float)
 args = ap.parse_args()
 
@@ -20,16 +22,32 @@ t = np.linspace(
 )
 frequency = args.frequency
 
+def saw(t, f, algorithm):
+    if algorithm == "segments":
+        nseg = int(sample_rate / f)
+        seg = np.linspace(-1, 1, nseg)
+        s = np.tile(seg, int(nsamples / nseg))
+    elif algorithm == "mod":
+        s = (t * 2.0 * frequency) % 2.0 - 1.0
+    elif algorithm == "floor":
+        # https://en.wikipedia.org/wiki/Sawtooth_wave
+        p = 1.0 / f
+        s = 2.0 * (t / p - np.floor(0.5 + t / p))
+    else:
+        assert False
+    return s
+
 if args.signal == "sine":
     s = np.sin(2 * np.pi * frequency * t)
-elif args.signal == "saw-rise":
-    nseg = int(sample_rate / frequency)
-    seg = np.linspace(-1, 1, nseg)
-    s = np.tile(seg, int(nsamples / nseg))
-elif args.signal == "saw-fall":
-    nseg = int(sample_rate / frequency)
-    seg = np.linspace(1, -1, nseg)
-    s = np.tile(seg, int(nsamples / nseg))
+elif args.signal == "saw":
+    s = saw(t, frequency, args.algorithm)
+else:
+    assert False
+
+if args.phase == "rise":
+    pass
+elif args.phase == "fall":
+    s = -s
 else:
     assert False
 
